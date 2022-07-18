d7e9bbb0bec4085bbe828c3638af5ab2f2b9c2b6
Gaikawade <gmhs6199@gmail.com>
2022-07-18 16:10:36 +0530

    update

--- src/controller/urlController.js
+++ src/controller/urlController.js
@@ -5,20 +5,28 @@ const baseURL = "localhost:3000";
 
 const postURL = async (req, res) => {
     try{
-        let data = req.body;
-        let {longUrl} = data;
+        let urlBody = req.body;
+        let {longUrl} = urlBody;
 
         if(!longUrl) return res.status(400).send({status: false, message: 'Long URL is mandatory'});
 
+        const lUrlExists = await urlModel.findOne({longUrl: longUrl});
+        if(lUrlExists) return res.status(400).send({status: false, message: 'You have already shorten this URL', shortUrl: lUrlExists.shortUrl});
+
         const urlCode = shortid.generate();
+        const shortUrl = baseURL + '/' + urlCode;
+
+        const sUrlExists = await urlModel.findOneAndDelete({shortUrl: shortUrl});
+        if(sUrlExists) return res.status(400).send({status: false, message: 'Short URL already exists'});
+
         const obj = {
             urlCode: urlCode,
-            shortUrl: baseURL + '/' + urlCode,
+            shortUrl: shortUrl,
             longUrl: longUrl
         }
 
         await urlModel.create(obj);
-        let urlData = await urlModel.findOne(obj).select({urlCode: 1, shortUrl: 1, longUrl: 1});
+        let urlData = await urlModel.findOne(obj).select({urlCode: 1, shortUrl: 1, longUrl: 1, _id: 0});
         res.status(201).send({status: true, message: urlData});
     }
     catch(err){
@@ -27,13 +35,18 @@ const postURL = async (req, res) => {
 }
 
 const getURL = async (req, res) => {
-    const {urlCode} = req.params.urlCode;
-    const find = await urlModel.findOne({urlCode: urlCode});
+    try{
+        const urlCode = req.params.urlCode;
+        const find = await urlModel.findOne({urlCode: urlCode});
 
-    if(find){
-        return res.status(302).redirect(find.longUrl);
-    }else{
-        return res.status(404).send({status: false, message: 'URL not found'});
+        if(find){
+            return res.status(302).redirect(find.longUrl);
+        }else{
+            return res.status(404).send({status: false, message: 'URL not found'});
+        }
+    }
+    catch(err){
+        res.status(500).json({status: false, message: err.message});
     }
 }
 
